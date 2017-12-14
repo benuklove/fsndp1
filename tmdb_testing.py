8254a5e36ef9a709caeee6946ed85e8536350c60
benlove <benuklove@gmail.com>
2017-12-14 10:47:22 -0600

    Next phase of movie API calls - getting info for each movie.

--- tmdb_testing.py
+++ tmdb_testing.py
@@ -1,33 +1,21 @@
 import requests
 import apikey
 
-base_url = "https://api.themoviedb.org/3/search/movie"
-# url = "https://api.themoviedb.org/3/search/movie"
+
 key = apikey.apikey
-lang = "&language=en-US&query="
 
 query = "muppets"
 
-end = "?api_key=" + key + "&language=en-US&query=" + query + "&page=1&include_adult=false"
-
-# url = [key, lang, query, end]
-
-
-# url = "".join([base_url, end])
-#
-# payload = {}
-# response = requests.request("GET", url, data=payload)
 url = "https://api.themoviedb.org/3/search/movie?api_key={}&language=en-US&query=" \
       "{}&page=1&include_adult=false".format(key, query)
 
 r = requests.get(url)
-# print(type(r))
+
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 data = r.json()
 # print(pp.pprint(data["results"][0]))
-# print(pp.pprint(data))
-# print(len(data["results"]))
+
 movies = []
 # for result in data["results"]:
 #     # d = {}
@@ -35,13 +23,31 @@ movies = []
 #     d = dict(id=result["id"], poster=result["poster_path"])
 #     movies.append(d)
 
-[movies.append(dict(id=result["id"], poster=result["poster_path"])) for result in data["results"]]
-# print(movies)
-# print(type(movies[0]["id"]))
-# vurl = "https://api.themoviedb.org/3/movie/{}?api_key={}".format(movies[0]["id"], key)
+# Add movies to the list
+[movies.append(dict(id=result["id"], poster=result["poster_path"], popularity=result["popularity"]))
+ for result in data["results"]]
 
 vurl = "http://api.themoviedb.org/3/movie/{}?api_key={}&append_to_response=videos"\
     .format(157336, key)
 vr = requests.get(vurl)
 vdata = vr.json()
-print(pp.pprint(vdata))
+# print(pp.pprint(vdata["videos"]["results"][0]["key"]))
+
+# Shorten longer list of movies that results from common movie title keywords
+
+
+# for each dict, get and add youtube key
+for movie in movies:
+    vurl = "http://api.themoviedb.org/3/movie/{}?api_key={}&append_to_response=videos"\
+        .format(movie["id"], key)
+    vr = requests.get(vurl)
+    vdata = vr.json()
+    # for simplicity, use the first movie trailer option
+    if vdata["videos"]["results"]:
+        youtube_key = vdata["videos"]["results"][0]["key"]
+    else:
+        youtube_key = "None"
+    movie["youtube_key"] = youtube_key
+
+print(pp.pprint(movies))
+
