e8192465da09c35bc82dcc9050b2edcdcfd87370
benlove <benuklove@gmail.com>
2017-12-13 19:52:38 -0600

    reformat structure of files, add main function.

---
+++ entertainment_center.py
@@ -0,0 +1,10 @@
+import fresh_tomatoes
+import my_favorites
+
+
+def main():
+    fresh_tomatoes.open_movies_page(my_favorites.movies)
+
+
+if __name__ == '__main__':
+    main()
---
+++ my_favorites.py
@@ -0,0 +1,27 @@
+import media
+
+
+holy_grail = media.Movie("Monty Python and the Holy Grail",
+                         "Another film completely different than some of the other films which aren't\
+                          quite the same as this on is",
+                         "https://upload.wikimedia.org/wikipedia/en/0/08/Monty-Python-1975-poster.png",
+                         "https://www.youtube.com/watch?v=LG1PlkURjxE"
+                         )
+
+spaceballs = media.Movie("Spaceballs",
+                         "Once upon a time warp in deep space, the struggle between the nice & the rotten goes on...",
+                         "https://upload.wikimedia.org/wikipedia/en/4/45/Spaceballs.jpg",
+                         "https://www.youtube.com/watch?v=RUnhzwnn_Q0")
+
+naked_gun = media.Movie("Naked Gun",
+                        "From the files of Police Squad",
+                        "https://upload.wikimedia.org/wikipedia/en/6/68/Nakedguntrilogy.jpg",
+                        "https://www.youtube.com/watch?v=PACTQutbow4")
+
+hot_shots = media.Movie("Hot Shots",
+                        "Unprecedented! Uncut! Under ten dollars! The mother of all movies!",
+                        "https://upload.wikimedia.org/wikipedia/en/b/b5/Hot_Shots_2.jpg",
+                        "https://www.youtube.com/watch?v=jGpOLb_PwYU")
+
+
+movies = [holy_grail, spaceballs, naked_gun, hot_shots]
