b032a74187479b1742481932e775b8ba423dc698
benlove <benuklove@gmail.com>
2017-12-14 11:51:15 -0600

    Limit movie title keyword query result length, plus some clean up.

--- tmdb_testing.py
+++ tmdb_testing.py
@@ -1,48 +1,39 @@
 import requests
 import apikey
+from operator import itemgetter
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 key = apikey.apikey
-
 query = "muppets"
 
 url = "https://api.themoviedb.org/3/search/movie?api_key={}&language=en-US&query=" \
       "{}&page=1&include_adult=false".format(key, query)
 
 r = requests.get(url)
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
 data = r.json()
 # print(pp.pprint(data["results"][0]))
 
 movies = []
-# for result in data["results"]:
-#     # d = {}
-#     # d["id"] = result["id"]
-#     d = dict(id=result["id"], poster=result["poster_path"])
-#     movies.append(d)
 
 # Add movies to the list
-[movies.append(dict(id=result["id"], poster=result["poster_path"], popularity=result["popularity"]))
- for result in data["results"]]
+[movies.append(dict(title=result["original_title"],
+                    id=result["id"],
+                    poster=result["poster_path"],
+                    popularity=result["popularity"])) for result in data["results"]]
 
-vurl = "http://api.themoviedb.org/3/movie/{}?api_key={}&append_to_response=videos"\
-    .format(157336, key)
-vr = requests.get(vurl)
-vdata = vr.json()
-# print(pp.pprint(vdata["videos"]["results"][0]["key"]))
+# Longer list of movies that results from common movie title keywords, shortened to 20 most popular
+movies = sorted(movies, key=itemgetter("popularity"), reverse=True)[:20]
 
-# Shorten longer list of movies that results from common movie title keywords
-
-
-# for each dict, get and add youtube key
+# For each dict, get and add youtube key
 for movie in movies:
     vurl = "http://api.themoviedb.org/3/movie/{}?api_key={}&append_to_response=videos"\
         .format(movie["id"], key)
     vr = requests.get(vurl)
     vdata = vr.json()
-    # for simplicity, use the first movie trailer option
+    # for simplicity, use the first movie trailer option available for each movie
     if vdata["videos"]["results"]:
         youtube_key = vdata["videos"]["results"][0]["key"]
     else:
@@ -50,4 +41,3 @@ for movie in movies:
     movie["youtube_key"] = youtube_key
 
 print(pp.pprint(movies))
-
